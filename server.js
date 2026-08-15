a0931b6606ce729d277fca1577cc678a9f02ee9b
HMMS459 <hmms82658753@gmail.com>
2026-08-15 21:17:44 +0500

    Updated HTML structure at 21:17

--- server.js
+++ server.js
@@ -102,4 +102,4 @@ app.delete('/api/challans', async (req, res) => {
 
 // Server Initialization
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port`));
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
